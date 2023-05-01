6a6e7b02bd7709863e9c8ead5e11c6b598ba9eb4
RenanLMenezes <renanlmenezes2003@gmail.com>
2023-05-01 10:37:35 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Renan"
+	var numAgencia int = 0001
+	var numConta int = 123456
+	var saldo float64 = 100.5
+
+	fmt.Println(titular, numAgencia, numConta, saldo)
+}
