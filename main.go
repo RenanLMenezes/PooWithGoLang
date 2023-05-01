fec74884468516bfd12e64e6648aaeb4af1102b2
RenanLMenezes <renanlmenezes2003@gmail.com>
2023-05-01 11:28:55 -0300

    Pointer

--- main.go
+++ main.go
@@ -14,4 +14,9 @@ func main() {
 	cliente1 := ContaCorrente{titular: "Renan", saldo: 100.5}
 	cliente2 := ContaCorrente{"Jão", 0002, 123457, 0}
 	fmt.Println(cliente1, cliente2)
+
+	var cliente3 *ContaCorrente // pointer
+	cliente3 = new(ContaCorrente)
+	cliente3.titular = "Julio"
+	fmt.Println(cliente3)
 }
